a80e545f4eb96a695ea560d2947acbfa55b61372
LiuYang <651636074@qq.com>
2016-03-02 08:14:56 +0800

    Initial Commit

---
+++ SVM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SVM
+//
+//  Created by 刘洋 on 16/3/2.
+//  Copyright © 2016年 Louie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
